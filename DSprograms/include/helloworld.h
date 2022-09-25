aeb5ea5a0b7230804b6b4b35d4eaa6a90e98af84
Suyog <suyogyj@gmail.com>
2022-09-25 15:49:42 +0530

    Added folder

---
+++ DSprograms/include/helloworld.h
@@ -0,0 +1,16 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
+class helloworld
+{
+    public:
+        helloworld();
+        virtual ~helloworld();
+
+    protected:
+
+    private:
+};
+
+#endif // HELLOWORLD_H
