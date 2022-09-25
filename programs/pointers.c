670d9017392e43a437489364135e4a7f5b114c12
Suyog <suyogyj@gmail.com>
2022-09-25 17:40:14 +0530

    experiments with pointers

--- programs/pointers.c
+++ programs/pointers.c
@@ -3,11 +3,23 @@
 
 int main(){
 
-	int x = 3;
+	int x = 4;
 	int *y;
+	int z = 3;
 	y = &x;
-	printf("%d \n",x);
-    printf("%p", y);
+//    printf("%d \n",x);
+//    printf("%p\n", y);
+//
+//
+//    printf("%d\n", *y);
+//    printf("%p\n", &x);
+//
+//    printf("%d\n", z);
+//    printf("%p", &z);
+
+    *y = *y+1;
+    printf("%d \n",x);
+
 	return 0;
 
 }
