645cefc99d54d3f0589f3a61e030c1a7fa6983f3
Suyog <suyogyj@gmail.com>
2022-09-26 19:34:22 +0530

    stuff in class
    pointers, linked lists, customised data types

--- programs/customDataTypes.c
+++ programs/customDataTypes.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct nodeType{
+        int value;
+        struct nodeType * next;
+
+};
+
+typedef struct nodeType Node
+
+int main(int argc, char **argv){
+    int i,val;
+    Node * head = NULL;
+    Node * temp = NULL;
+
+    i = 0;
+    scanf("%d", &val);
+    while(val>0){
+        temp = malloc(sizeof(Node));
+        temp->value = val;
+        temp->next = NULL;
+        // if this is the first time
+        // or else
+        i = i+1;
+        scanf("%d", &val);
+
+    }
+}
