b29f55bcb96d9513d5d1b0d68b812a0b11ecc76c
Suyog <suyogyj@gmail.com>
2022-11-07 21:15:52 +0530

    added tortoiseHare function to print half of the linked list

---
+++ tas-look-here/printHalfLL.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node* nextPtr;
+};
+typedef struct node listNode;
+typedef listNode* nodePtr;
+
+//insert a new node in the linked list
+
+void insert(listNode** head, int num){
+
+    listNode* newPtr = malloc(sizeof(struct node));
+
+    newPtr->value = num;
+    newPtr->nextPtr = NULL;
+
+    if(*head == NULL)
+    {
+        *head = newPtr;
+        return;
+    }
+
+    listNode *temp = *head;
+    
+    while(temp->nextPtr != NULL)
+    {
+        temp = temp->nextPtr;
+    }
+
+    temp->nextPtr = newPtr;
+
+}
+
+
+void printLinkedList(nodePtr p){
+
+    while(p != NULL){
+        printf("%d ,", p->value);
+        p = p->nextPtr;
+    }
+}
+
+void tortoiseHare(listNode* head)
+{
+    listNode* tort = head;
+    listNode* hare = head;
+
+    while(hare->nextPtr != NULL)
+    {
+        printf("%d ,", tort->value);
+        tort = tort->nextPtr;
+        hare = hare->nextPtr->nextPtr;
+        
+    }
+}
+
+int main() {
+    nodePtr head = NULL;
+    int item;
+    int length;
+    printf("Input length of list: ");
+    scanf("%d", &length);
+    int i;
+    for(i = 0; i < length; i++){
+        printf("Enter a number: ");
+        scanf("%d", &item);
+        insert(&head, item);
+    }
+    tortoiseHare(head);
+    printf("\n");
+}
